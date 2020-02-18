0347bcfa89411165cca4de93452d8b0f8d3c95df
Italo Rayone <kirito@eniac.mds.net>
2020-02-18 19:26:13 -0300

    Initial commit

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        {/* <Label color="#e83f6f" /> */}
+      </header>
+      <p>Fazer a migração completa de servidor</p>
+      <img src="" alt="" />
+    </Container>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+  height: 80px;
+  display: flex;
+  align-items: center;
+  font: 14px 'Roboto' sans-serif;
+  background: #e83f6f;
+  color: #fff;
+
+`;
